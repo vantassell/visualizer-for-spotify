8d2f604b4da7a9e0e0371f9f8758b1f4d8a2c5a2
Andrew Van Tassell <vantassell@gmail.com>
2023-08-04 19:33:27 -0700

    remove redirect pause

--- src/login.js
+++ src/login.js
@@ -9,25 +9,25 @@ const accessToken = Cookies.get("accessToken");
 const displayName = Cookies.get("displayName");
 
 if (accessToken) {
-  document.querySelector(".login-result").innerHTML = `
-    Success! Signed in as ${displayName}. Launching your visualizer now!!
-  `;
+  // document.querySelector(".login-result").innerHTML = `
+  //   Success! Signed in as ${displayName}. Launching your visualizer now!!
+  // `;
 
   const baseURL = window.location.toString().split('/login')[0];
   console.log(`baseURL: ${baseURL}`);
 
-  setTimeout(() => {
+  // setTimeout(() => {
     const redirectTo = baseURL + '/players/basic';
     console.log(`redirecting to: ${redirectTo}`)
     window.location.replace(redirectTo);
-  }, 4000);
+  // }, 4000);
 } else {
-  document.querySelector(".login-result").innerHTML = `
-    Failed to login, returning to home page.
-`;
-  setTimeout(() => {
+//   document.querySelector(".login-result").innerHTML = `
+//     Failed to login, returning to home page.
+// `;
+//   setTimeout(() => {
     const redirectTo = baseURL + '/';
     console.log(`redirecting to: ${redirectTo}`)
     window.location.replace(redirectTo);
-  }, 2000);
+  // }, 2000);
 }
