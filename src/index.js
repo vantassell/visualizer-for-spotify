d133b3eae007909c56f19912c43b7c91d8a8beef
Andrew Van Tassell <vantassell@gmail.com>
2023-08-04 22:09:09 -0700

    Fix path for player link

--- src/index.js
+++ src/index.js
@@ -15,24 +15,22 @@ videoSource.setAttribute("src", `${star_loop_uri}`);
 const videoPlayer = document.getElementById("video-player");
 videoPlayer.appendChild(videoSource);
 
-console.log(window.history.state);
-
 
 // check if they're signed in
 if (accessToken && displayName) {
   console.log(`signed in with accessToken: ${accessToken}`);
+  const baseURL = window.location.toString();
+  // NOTE: this baseURL ends with a / because there's nothing to split on.
+  const redirectTo = baseURL + "players/basic";
+  console.log(`redirectTo: ${redirectTo}`);
+  // window.location.replace(redirectTo);
+
+
   const playerLink = document.querySelector(".go-to-player");
   playerLink.style.display = "block";
   playerLink.innerHTML = `
-      <a href="/players/basic.html">Go to ${displayName}'s <span class=go-to-player__player>Player</span></a>
+      <a href="redirectTo">Go to ${displayName}'s <span class=go-to-player__player>Player</span></a>
 `;
-  const baseURL = window.location.toString();
-  console.log(`baseURL: ${baseURL}`);
-
-  // NOTE: this baseURL ends with a / because there's nothing to split on.
-  const redirectTo = baseURL + "players/basic";
-  console.log(`redirecting to: ${redirectTo}`);
-  // window.location.replace(redirectTo);
 }
 
 console.log("exiting index.js");
