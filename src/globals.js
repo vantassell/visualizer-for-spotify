88f125b7094654490dd489cc21997d052b98d935
Andrew Van Tassell <vantassell@gmail.com>
2024-05-05 09:59:51 -0700

    update to digital ocean

--- src/globals.js
+++ src/globals.js
@@ -1,3 +1,4 @@
 export const INITIAL_POLLING_INTERVAL = 1000;
-export const API_PLAYERS_URL = "https://avt-sv-api-16ae49589f38.herokuapp.com/visualizers";
+// export const API_PLAYERS_URL = "https://avt-sv-api-16ae49589f38.herokuapp.com/visualizers";
+export const API_PLAYERS_URL = "https://king-prawn-app-eclfh.ondigitalocean.app/visualizers";
 export const LOGGING_LEVEL = 2; // 1 - minimal, 2 - verbose
